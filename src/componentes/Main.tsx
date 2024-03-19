be93b056d16d3876874fbf8e1a49c7a2e8f5bb61
camilahsp <116742754+camilahsp@users.noreply.github.com>
2024-03-19 10:14:33 -0400

    Box Estudante na Main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,10 +1,22 @@
+import './Main.css'
+
 function Main(){
     return(
-        <>
-        <main>Camila2</main>
-        <h1>Meu H1</h1>
-        </>
+
+        <main>
+            <div className='estudante-box'>
+                <img className="estudante-foto" src="https://github.com/camilahsp.png" alt=""></img>
+                <div className='informacoes'>
+                <h3 className='estudante-nome'>Camila Haiane</h3>
+                <p className='estudante-descricao'>Estudante do IFMS, curso de informática. No momento, está estagiando no MPMS</p> 
+                <a className='link-git' href="https://github.com/camilahsp">GitHub</a>
+                </div>
+            </div>
+            
+             </main>
+
     )
+
 }
 
 export default Main;
